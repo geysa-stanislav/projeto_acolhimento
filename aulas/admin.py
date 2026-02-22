a745e80ed937a4724b8d8b924879a40b6ce3ff5b
geysa-stanislav <martins.geysa01@gmail.com>
2026-02-22 19:39:32 -0400

    Atualizando link do Instagram

--- aulas/admin.py
+++ aulas/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry # <-- A mágica importada aqui
 from .models import Licao
 
+# --- CONFIGURAÇÃO DAS LIÇÕES (O QUE VOCÊ JÁ TINHA) ---
 @admin.register(Licao)
 class LicaoAdmin(admin.ModelAdmin):
     # Colunas que aparecerão na lista (muito mais profissional)
@@ -18,3 +20,20 @@ class LicaoAdmin(admin.ModelAdmin):
     def visualizar_no_site(self, obj):
         # Cria um link rápido para o admin ver como está a lição no site
         return "Visualizar"
+
+# --- O NOVO CÓDIGO DO RAIO-X (AUDITORIA DE ADMINS) ---
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    # Mostra: Data/Hora, Quem fez, Onde fez, Qual arquivo, e O que fez (Adicionou/Editou/Apagou)
+    list_display = ['action_time', 'user', 'content_type', 'object_repr', 'action_flag']
+    
+    # Cria filtros na lateral por usuário e por tipo de ação
+    list_filter = ['user', 'action_flag']
+    
+    # Barra de pesquisa
+    search_fields = ['object_repr', 'change_message']
+
+    # Bloqueia a edição dessa tela (ninguém pode apagar provas do que fez! rs)
+    def has_add_permission(self, request): return False
+    def has_change_permission(self, request, obj=None): return False
+    def has_delete_permission(self, request, obj=None): return False
