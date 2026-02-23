681b080b5616786fa8ec622d93f719d2675d56bc
geysa-stanislav <martins.geysa01@gmail.com>
2026-02-22 21:28:23 -0400

    Detalhes dos cards

--- guia/views.py
+++ guia/views.py
@@ -5,8 +5,11 @@ def index_guia(request):
     categorias = Categoria.objects.all()
     return render(request, 'guia/index.html', {'categorias': categorias})
 
+# CÓDIGO CORRETO:
 def detalhe_categoria(request, id):
     categoria = get_object_or_404(Categoria, id=id)
-    # Garante que as publicações sejam encontradas para a página não dar erro
-    publicacoes = Publicacao.objects.filter(categoria=categoria)
-    return render(request, 'guia/categoria.html', {'categoria': categoria, 'publicacoes': publicacoes})
+    
+    # Agora ele filtra e pega SÓ as publicações que pertencem a essa categoria!
+    publicacoes = Publicacao.objects.filter(categoria=categoria) 
+    
+    return render(request, 'categoria.html', {'categoria': categoria, 'publicacoes': publicacoes})
